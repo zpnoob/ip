8562bfdffc92c34b85ae07d5510d2f621226cbce
zamien <pehzamie@gmail.com>
2025-08-20 21:50:06 +0800

    Level 0: Renamed bot, add greeting and exit messages

---
+++ src/main/java/Jung.java
@@ -0,0 +1,9 @@
+public class Jung {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Jung.");
+        System.out.println("What can I do for you?");
+
+        //exit for now
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
