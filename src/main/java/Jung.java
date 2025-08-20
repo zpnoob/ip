84b842b1a5baaadbdc0ac734a21539d8957922df
zamien <pehzamie@gmail.com>
2025-08-21 00:39:53 +0800

    Level 3:Mark and unmark

--- src/main/java/Jung.java
+++ src/main/java/Jung.java
@@ -9,7 +9,7 @@ public class Jung {
         //up till this point nothing should be changed here anymore
 
         //given that there is no more than 100 task so can use String[100]
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount =  0;
 
         while (true) {
@@ -18,16 +18,33 @@ public class Jung {
             if (input.equals("bye")) {
                 System.out.println("Bye. I hope I never see you again..");
                 break;
-            } else if (input.equals("list")){
+            } else if (input.equals("list")) {
                 //otherwise repeat the tasks to them and number them if the user inputs "list"
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                     //must start from i = 0 oops because otherwise, when u run it starts from the 2nd
                     //item that you add to the list if start from i = 1
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                //get the substring starting from char at index 5 till end, then trim away
+                //then convert the remaining int into string, -1 is for the indexing later on
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsDone();
+                    System.out.println("Lame, this task is marked as done.");
+                    System.out.println(tasks[index]);
+                }
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsNotDone();
+                    System.out.println("I've marked this task as not done yet.");
+                    System.out.println(tasks[index]);
+                }
             } else {
                 // keep adding in tasks otherwise
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
