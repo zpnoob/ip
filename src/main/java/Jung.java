403e6e34b832ae57ad430adfae95cf9e0167297d
zamien <pehzamie@gmail.com>
2025-08-20 22:16:07 +0800

    Level 1: Echoing inputs

--- src/main/java/Jung.java
+++ src/main/java/Jung.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Jung {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //scans in any input by user
+
         System.out.println("Hello! I'm Jung.");
         System.out.println("What can I do for you?");
 
-        //exit for now
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(input);
+            //exit if user says bye
+            if (input.equals("bye")) {
+                System.out.println("Bye. I hope I never see you again..");
+                break;
+            }
+        }
     }
 }
