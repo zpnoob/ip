faf3503e74630dce3996c5eb5f8ef625009a7639
zamien <pehzamie@gmail.com>
2025-08-21 21:48:42 +0800

    Level 4: Events class added

---
+++ src/main/java/Event.java
@@ -0,0 +1,24 @@
+public class Event extends Task {
+    private String from;
+    private String to;
+    //these additional fields required under Events.
+
+    public Event(String description, String from, String to) {
+        super(description, 'E');
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (from: " + from + "to: " + to + ")";
+    }
+}
