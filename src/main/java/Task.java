cb7492abc872e708b369581970bc7334d7426e03
zamien <pehzamie@gmail.com>
2025-08-21 21:09:01 +0800

    Level 4: Making Task.java abstract first

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+public abstract class Task {
+    protected String description;
+    protected boolean isDone;
 
-public class Task {
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); //marks done task with X
+    }
+
+    //next 2 methods are to change the boolean for whether a task is done
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    //need to add a toString to represent [X] or [ ]
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
 }
