fc126f18cb09c4b1f3ca6b7d51620f6c41e0832b
zamien <pehzamie@gmail.com>
2025-08-21 21:36:58 +0800

    Level 4: ToDo class added

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,8 @@
+public class ToDo extends Task {
+
+    public ToDo(String description) {
+        super(description, 'T');
+    }
+
+    //class has no extra requirements/fields so can just take the Task's toString method
+}
