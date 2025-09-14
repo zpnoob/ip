f137689343ceacc4eebcefaece4a45286ba4e9d5
zamien <pehzamie@gmail.com>
2025-09-14 15:52:15 +0800

    Create more tests for tasks

--- src/test/java/jung/storage/TaskListTest.java
+++ src/test/java/jung/storage/TaskListTest.java
@@ -67,4 +67,70 @@ public class TaskListTest {
     public void unmarkTask_invalidIndex_throwsJungException() {
         assertThrows(JungException.class, () -> taskList.unmarkTask(0));
     }
+    @Test
+    public void deleteTask_negativeIndex_throwsJungException() {
+        assertThrows(JungException.class, () -> taskList.deleteTask(-1));
+    }
+
+    @Test
+    public void deleteTask_indexTooLarge_throwsJungException() throws IOException {
+        taskList.addTask(new ToDo("test"));
+        assertThrows(JungException.class, () -> taskList.deleteTask(1));
+    }
+
+    @Test
+    public void markTask_negativeIndex_throwsJungException() {
+        assertThrows(JungException.class, () -> taskList.markTask(-1));
+    }
+
+    @Test
+    public void unmarkTask_negativeIndex_throwsJungException() {
+        assertThrows(JungException.class, () -> taskList.unmarkTask(-1));
+    }
+
+    @Test
+    public void findTasksByKeyword_existingKeyword_returnsMatchingTasks() throws IOException {
+        taskList.addTask(new ToDo("buy groceries"));
+        taskList.addTask(new ToDo("buy books"));
+        taskList.addTask(new ToDo("sell items"));
+
+        ArrayList<Task> results = taskList.findTasksByKeyword("buy");
+
+        assertEquals(2, results.size());
+        assertEquals("buy groceries", results.get(0).getDescription());
+        assertEquals("buy books", results.get(1).getDescription());
+    }
+
+    @Test
+    public void findTasksByKeyword_nonExistentKeyword_returnsEmptyList() throws IOException {
+        taskList.addTask(new ToDo("test task"));
+
+        ArrayList<Task> results = taskList.findTasksByKeyword("nonexistent");
+
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    public void findTasksByKeyword_caseInsensitive_returnsMatchingTasks() throws IOException {
+        taskList.addTask(new ToDo("Buy Groceries"));
+
+        ArrayList<Task> results = taskList.findTasksByKeyword("buy");
+
+        assertEquals(1, results.size());
+        assertEquals("Buy Groceries", results.get(0).getDescription());
+    }
+
+    @Test
+    public void size_multipleOperations_returnsCorrectSize() throws IOException, JungException {
+        assertEquals(0, taskList.size());
+
+        taskList.addTask(new ToDo("task 1"));
+        assertEquals(1, taskList.size());
+
+        taskList.addTask(new ToDo("task 2"));
+        assertEquals(2, taskList.size());
+
+        taskList.deleteTask(0);
+        assertEquals(1, taskList.size());
+    }
 }
